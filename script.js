a592870da6de15fd35f52105217893f82526eee3
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-07-31 15:57:11 -0300

    .

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
        const botaoAlternativas = document.createElement("button");
        botaoAlternativas.textContent = alternativa;
-       caixaAlternativas.appendChild(botaoAlternativas) 
+       caixaAlternativas.appendChild(botaoAlternativas); 
     }
 }
 
