8b0450d7f403f9ce397c74adf2c7bcb6eaf23770
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-07-31 16:06:55 -0300

    .

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-Alernativas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Você acredita que  o conhecimento da cultura desenvolvida nas escola indígenas pode melhorar o entendimento da comunidade em relação às mudanças climáticas atuais ?",
+        enunciado: "Quais são as  soluções que a escola está buscando  para a inclusão social, bem como as questões relacionadas aos direitos dos povos originários e como isso afeta os impactos causados pelo desmatamento ?",
         alternativas: [
-            "Sim, eu acredito que pode ser uma grande contribuição", 
-            "Não, pois o método empregado não é benéfico para o meio ambiente "       
+            "Técnicas alternativas inovadoras", "Ensino multidisciplinar"
         ]
     },
     {
         enunciado: "Qual o papel fundamental dos povos indígenas na luta contra a crise climática e preservação ambiental?",
         alternativas: [
-            "Eles têm mostrado que é possível preservar a biodiversidade, manter os serviços ecossistêmicos e acalmar os efeitos das mudanças climáticas.",
-            "Eles protegem e promovem a biodiversidade através de práticas tradicionais de manejo sustentável dos recursos naturais."       
+            "Eles têm mostrado que é possível preservar a biodiversidade, manter os serviços ecossistêmicos e acalmar os efeitos das mudanças climáticas.", 
+            "Eles protegem e promovem a biodiversidade através de práticas tradicionais de manejo sustentável dos recursos naturais."
         ]
     },
     {
-        enunciado: "Quais são as  soluções que a escola está buscando  para a inclusão social, bem como as questões relacionadas aos direitos dos povos originários e como isso afeta os impactos causados pelo desmatamento ?",
+        enunciado: "Você acredita que  o conhecimento da cultura desenvolvida nas escola indígenas pode melhorar o entendimento da comunidade  em relação às mudanças climáticas atuais ?",
         alternativas: [
-            "Técnicas alternativas inovadoras", 
-            "Ensino multidisciplinar"       
+            "Sim, eu acredito que pode ser uma grande contribuição .", 
+            "Não, pois o método empregado não é benéfico para o meio ambiente ."
         ]
     }
 ];
