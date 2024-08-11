dc1b84b8b64322f6132392ce94ee4e3b16f91267
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-08-11 19:26:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener('click',function(){
+        botaoAlternativas.addEventListener("click", function(){
             atual++
             mostraPergunta();
         });
